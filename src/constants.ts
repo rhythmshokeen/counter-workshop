031dc2f446c1785ebf485e4ee0e24535c2546be3
Rhythm Shokeen <imrhythmshokeen@gmail.com>
2025-09-05 11:13:57 +0000

    initial commit

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const COUNTER_PACKAGE_ID = "0x89f68c4e80d22ef60677b9e23ab4a240e8dc8bd4bbedab3b7d3d3775311c77aa";
+export const COUNTER_PACKAGE_ID = "0xaf0429882cd723bfb85f54bb20beb6f2ff41056fe10ab9c4a93075e84f2c03d5";
 
